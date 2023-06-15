8c44e4ae72e8ab9113cfd2699934c0b5c50ff2fe
Ali Berat Tatli <berat.tatli@prodyum.com>
2023-06-16 01:12:31 +0300

    Index.js Arrangements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
+import CartTotals from "./components/cart/CartTotals";
+import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
+import Products from "./components/products/Products";
 
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      
-    </div>
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories overflow-auto max-h-[calc(100vh-_-112px)] pb-72">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <Products />
+        </div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] border md:-mt-[24px]">
+          <CartTotals />
+        </div>
+      </div>
+    </>
   );
 }
 
