449e76bc609b4cb7f16e6dca41687a658f53dd31
Ali Berat Tatli <berat.tatli@prodyum.com>
2023-06-18 00:13:53 +0300

    Home Page Responsive Arrangements Added

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
           />
 
         </div>
-        <div className="menu-links flex justify-between items-center gap-7 md:static fixed bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
+        <div className="menu-links flex justify-between items-center gap-7 md:static fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
           <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff] transition-all">
             <HomeOutlined className="md:tex-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Home</span>
