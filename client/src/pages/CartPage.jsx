e632fccc43e82a56681cdcf32673b3e66d514c61
Ali Berat Tatli <berat.tatli@prodyum.com>
2023-06-18 00:59:13 +0300

    Cart Page Created and designed

---
+++ client/src/pages/CartPage.jsx
@@ -0,0 +1,65 @@
+import { Button, Card, Table } from "antd";
+import Header from "../components/header/Header";
+
+const CartPage = () => {
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+  return (
+    <>
+      <Header />
+      <div className="px-6">
+        <Table dataSource={dataSource} columns={columns} bordered pagination={false} />
+        <div className="cart-total flex justify-end mt-4">
+          <Card className="w-72">
+            <div className="flex justify-between">
+                <span>Subtotal</span>
+                <span>549.00₺</span>
+            </div>
+            <div className="flex justify-between my-2">
+                <span>Tax Total %8</span>
+                <span className="text-red-600">42.23</span>
+            </div>
+            <div className="flex justify-between">
+                <b>Total</b>
+                <b>591.23₺</b>
+            </div>
+            <Button className="mt-4 w-full" type="primary" size="large">
+               Create Order  
+            </Button>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CartPage;
